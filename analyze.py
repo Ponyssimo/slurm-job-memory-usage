b939158cc1a2b4613710865ffee4436f7f807c44
Jeffrey Condell <jdcrc@rit.edu>
2024-08-06 13:50:31 -0400

    added requested memory calculation and filtering

--- analyze.py
+++ analyze.py
@@ -7,7 +7,7 @@ import sys
 
 USAGE_THRESHOLD = 25. # Percent usage value below which a job will be flagged
 TIME_THRESHOLD = dt.timedelta(minutes=5) # Run time below which job will be ignored
-REQUESTED_THRESHOLD = 50
+REQUESTED_THRESHOLD = 1024 * 50
 
 TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
@@ -15,24 +15,61 @@ def main():
     in_path = sys.argv[1]
     pd.set_option('display.max_rows', 9999)
     df = pd.read_csv(in_path)
+
+    in_path = sys.argv[2]
+    total_df = pd.read_csv(in_path)
+
+    percent_row = 0
+    amount_row = 0
+    offset = 0
+
+    percent_time = df.get("Time")
+    amount_time = total_df.get("Time")
+
+    while percent_time[percent_row] != amount_time[amount_row]:
+        if percent_time[percent_row] < amount_time[amount_row]:
+            percent_row += 1
+        else:
+            amount_row += 1
+    offset = percent_row - amount_row
+
     for (column_name, column_data) in df.items():
         if column_name == "Time":
             continue
+
         if np.nanmax(column_data.values) > USAGE_THRESHOLD:
             df.drop(columns=column_name, inplace=True)
+            continue
+
         time, running = runtime(df, column_data)
+
         if time < TIME_THRESHOLD:
             df.drop(columns = column_name, inplace=True)
-
-    for (column_name, column_data) in df.items():
-        if column_name == "Time":
             continue
-        print(f'{column_name}: {np.nanmax(column_data.values)}')
 
-        time, running = runtime(df, column_data)
+        i = percent_row
+        err = False
+        while math.isnan(column_data[i]):
+            i += 1
+        amount_col = total_df.get(column_name)
+        while amount_col[i - offset] == 0 or column_data[i] == 0:
+            i += 1
+            if i >= len(df.index) - 1 or i >= len(total_df.index) - offset - 1:
+                err = True
+                break
+        if not err:
+            mib_req = (amount_col[i - offset] / column_data[i]) * 100
+            if mib_req < REQUESTED_THRESHOLD:
+                df.drop(columns=column_name, inplace=True)
+                continue
+
+        print(f'{column_name}:')
         if running:
             print('\trunning')
-        print(f'\t{time}')
+        print(f'\t{np.nanmax(column_data.values)}% maximum usage')
+        if not err:
+            print(f'\t{mib_req / 1024:.2f} GiB requested')
+        print(f'\t{time}')     
 
 def runtime(df, column_data):
     i = 0
